9191f9a37863ad77a1ef379e2575cc6a6c73afe9
Syncretic-secular-society <syedy5010@gmail.com>
2026-05-11 16:12:37 +0530

    feat(subkernel): export hotswap module + HotSwapCoordinator

--- kernel/timux/src/subkernel/mod.rs
+++ kernel/timux/src/subkernel/mod.rs
@@ -1,11 +1,14 @@
 extern crate alloc;
-pub mod instance;
-pub mod manager;
-pub mod bridge;
-pub mod snapshot;
-pub mod shadow;
+  pub mod instance;
+  pub mod manager;
+  pub mod bridge;
+  pub mod snapshot;
+  pub mod shadow;
+  pub mod hotswap;
 
-pub use instance::{SubKernel, SubKernelConfig, SubKernelId, SubKernelProfile, SubKernelState, MemoryRange};
-pub use manager::SubKernelManager;
-pub use bridge::{Bridge, BridgeKind};
-pub use shadow::{ShadowManager, ShadowInstance};
+  pub use instance::{SubKernel, SubKernelConfig, SubKernelId, SubKernelProfile, SubKernelState, MemoryRange};
+  pub use manager::SubKernelManager;
+  pub use bridge::{Bridge, BridgeKind};
+  pub use shadow::{ShadowManager, ShadowInstance};
+  pub use hotswap::HotSwapCoordinator;
+  
